3399144ffaa37908d1c7404b8391e65c2b94b447
ahmedkhaledelsayed <ahmed.khaled499453@gmial.com>
2023-08-10 13:59:49 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -  Entry point
+ *
+ * Description: a c program that print using put function
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
