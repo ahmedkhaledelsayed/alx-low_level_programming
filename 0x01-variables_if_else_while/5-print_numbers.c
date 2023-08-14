590c37c9475b0f961b638e68ced7ecaae03cbce7
ahmedkhaledelsayed <ahmed.khaled499453@gmial.com>
2023-08-14 13:54:08 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -  Entry point
+ *
+ * Description: a c program that prints the alphabet in lowercase
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int d = 0;
+
+	while (d < 10)
+	{
+		printf("%d", d);
+	}
+	putchar('\n');
+	return (0);
+}
