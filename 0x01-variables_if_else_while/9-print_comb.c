258e670ba75b964a69cca68d3f4cad5ffa032937
ahmedkhaledelsayed <ahmed.khaled499453@gmial.com>
2023-08-14 14:35:36 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -  Entry point
+ *
+ * Description: a c program that prints the numbers by separation
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int d = 0;
+
+	while (d <= 9)
+	{
+		putchar(d + '0');
+
+		if (d != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
