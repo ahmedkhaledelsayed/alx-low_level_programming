cb82b3e01b1de99eca267f62cf995b43b15a5932
ahmedkhaledelsayed <ahmed.khaled499453@gmial.com>
2023-08-15 15:28:39 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,21 +12,22 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
+		_putchar('0');
 		for (j = 0; j <= 9; j++)
 		{
 			if ((i * j) <= 9)
 			{
-				_putchar((i * j) + '0');
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
+				_putchar((i * j) + '0');
 			}
 			else
 			{
-				_putchar(((i * j) / 10) + '0');
-				_putchar(((i * j) % 10) + '0');
 				_putchar(',');
 				_putchar(' ');
+				_putchar(((i * j) / 10) + '0');
+				_putchar(((i * j) % 10) + '0');
 			}
 		}
 		_putchar('\n');
