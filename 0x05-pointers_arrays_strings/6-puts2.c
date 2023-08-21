d1946821386872bb8f9b952d6b4ab4304d2579ef
ahmedkhaledelsayed <ahmed.khaled499453@gmial.com>
2023-08-21 14:55:47 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* puts2 - function that prints every other character of a string
+*@str: pointer to string
+*/
+
+void puts2(char *str)
+{
+	int index;
+
+	for (index = 0; str[index]; index += 2)
+	{
+		printf("%c", str[index]);
+	}
+	printf("\n");
+}
