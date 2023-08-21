3bff16f43b75dde4b0f3810f8ba79ca32d219610
ahmedkhaledelsayed <ahmed.khaled499453@gmial.com>
2023-08-21 12:46:51 -0700

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _puts - function that returns the length of a string
+*@str: pointer to string
+*/
+
+void _puts(char *str)
+{
+	int index, counter = 0;
+
+	for (index = 0; s[index]; index++)
+	{
+		printf("%s", s[index]);
+	}
+}
