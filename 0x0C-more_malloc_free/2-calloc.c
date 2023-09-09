c6aebe32e382a2b9d7d5763d4947bdb9083b2bb2
ahmedkhaledelsayed <ahmed.khaled499453@gmial.com>
2023-09-08 19:30:29 -0700

    2. _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+* _memset - fills memory with a constant byte.
+*@s: pointer to put the constant
+*@b: constant .
+*@n: max bytes to use.
+*
+* Return: s
+*/
+
+void *_memset(char *s, char b, unsigned int n)
+{
+	char *p = s;
+
+	while (n--)
+	{
+		*s++ = b;
+	}
+	return (p);
+}
+/**
+* _calloc - a function that allocates memory for an array, using malloc.
+*@nmemb: array lenght
+*@size: integer for the size of array.
+*
+* Return: pointer
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *m;
+
+	if (size == 0 || nmemb == 0)
+	{
+		return (NULL);
+	}
+	malloc(sizeof(int) * nmemb);
+	if (m == 0)
+	{
+		return (NULL);
+	}
+	_memset(m, 0, sizeof(int) * nmemb);
+	return (m);
+}
