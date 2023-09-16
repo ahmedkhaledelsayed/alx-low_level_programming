23cfcc439a87ce8eb376a87a37b18789872c7ffe
ahmedkhaledelsayed <ahmed.khaled499453@gmial.com>
2023-09-16 04:38:12 +0300

    2. One woman's constant is another woman's variable

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,19 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * fm_t - struct fm
+ * @fm: pointer to char
+ * @p: pointer to function
+ * Return: none
+ */
+typedef struct fm
+{
+	char* fm;
+	void (*p)(char *, va_list);
+} fm_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
