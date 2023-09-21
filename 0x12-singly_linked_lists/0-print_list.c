1aab170e5a21041a2c50c3cbc6e0626812a67d56
ahmedkhaledelsayed <ahmed.khaled499453@gmial.com>
2023-09-21 21:49:48 +0300

    0-print_list: fix mistake in print

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,11 @@ size_t print_list(const list_t *h)
 
 	for (index = 0; h; index++)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
 		h = h->next;
 	}
 	return (index);
