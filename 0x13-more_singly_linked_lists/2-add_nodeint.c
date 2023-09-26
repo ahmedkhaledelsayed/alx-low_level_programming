4223a28db408b6f859b77e9659f06862495b3a73
ahmedkhaledelsayed <ahmed.khaled499453@gmail.com>
2023-09-26 10:16:15 -0700

    2. Add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_nodeint - function that adds a new node 
+ * add_nodeint - function that adds a new node
  * at the beginning of a listint_t list
  * @head: head of a list.
  * @n: n element.
