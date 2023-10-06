6250882b52b92fd5ee03631bfd1622926ed8f1c4
ahmedkhaledelsayed <ahmed.khaled499453@gmial.com>
2023-10-07 00:18:30 +0300

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,21 +8,27 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, lenght;
-	ssize_t res_write;
+	int fd, len;
+	ssize_t out_write;
 
 	if (filename == NULL)
+	{
 		return (-1);
+	}
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
+	{
 		return (-1);
+	}
 	if (text_content != NULL)
 	{
-		lenght = 0;
-		while (*(text_content + lenght) != '\0')
-			lenght++;
-		res_write = write(fd, text_content, lenght);
-		if (res_write == -1)
+		len = 0;
+		while (*(text_content + len) != '\0')
+		{
+			len++;
+		}
+		out_write = write(fd, text_content, len);
+		if (out_write == -1)
 		{
 			write(1, "fails", 6);
 			return (-1);
